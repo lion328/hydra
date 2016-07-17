39cb9612e37f32565c155c2dfbc0e6e818638c8d
Waritnan Sookbuntherng <lion328@hotmail.co.th>
2016-07-18 00:54:32 +0700

    Fix antialiasing problems

--- src/main/java/net/mc_warrior/launcher/Main.java
+++ src/main/java/net/mc_warrior/launcher/Main.java
@@ -5,6 +5,9 @@ public class Main
 
     public static void main(String[] args)
     {
+        System.setProperty("swing.aatxt", "true");
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+
         LauncherUI ui = new LauncherUI();
         ui.start();
     }
